4cf07e8270a127e8a82400f69058e63fab2d327f
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2023-11-21 17:51:50 +0100

    Implemented suboperators

---
+++ src/calculator/AdditionOperator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class AdditionOperator extends Operator {
+    public double operate(double operand1, double operand2) {
+        return operand1 + operand2;
+    }
+
+    @Override
+    void execute() {
+        ;
+    }
+}
---
+++ src/calculator/DivisionOperator.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class DivisionOperator extends Operator {
+    public double operate(double operand1, double operand2) {
+        return operand1 / operand2;
+    }
+
+    @Override
+    void execute() {
+        ;
+    }
+}
