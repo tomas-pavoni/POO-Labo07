3328cd1d538edbf1e56e3e23125b4f0610f581e6
Maxime-sch <55839375+Maxime-sch@users.noreply.github.com>
2023-11-29 11:39:30 +0100

    Fix MultiplicationOperator

--- src/calculator/MultiplicationOperator.java
+++ src/calculator/MultiplicationOperator.java
@@ -8,7 +8,7 @@ public class MultiplicationOperator extends Operator {
     @Override
     void execute() {
         if (!state.getStack().isEmpty() && !state.getCurrentValue().isEmpty() && state.getError().isEmpty()) {
-            state.setCurrentValue(Double.toString(state.getStack().pop() +
+            state.setCurrentValue(Double.toString(state.getStack().pop() *
                     Double.parseDouble(state.getCurrentValue())));
             state.nextNumberPushesToStack = true;
         }
